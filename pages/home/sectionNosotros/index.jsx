0eb5baf47271532ac4335b8f8f397485b6bdb480
Eduardo <centurionjaime@gmail.com>
2024-10-14 19:42:16 -0500

    Actualizar estilos y rutas de la sección "Nosotros"

--- pages/home/sectionNosotros/index.jsx
+++ pages/home/sectionNosotros/index.jsx
@@ -19,21 +19,24 @@ const SectionNosotros = () => {
             <div className={stylesSection.solutionsGrid}>
                 {/* Tarjeta de Transformación Digital 1 */}
                 <div className={stylesSection.solutionItem}>
-                    <Image src={fondo} alt="Transformación Digital"/>
+                    <Image
+                     src={require('../../../public/assets/img/home/sectionSomos/rocks-8283171_1920.jpg')} alt="Transformación Digital"/>
                     <h3>Transformación Digital</h3>
                     <p>Implementamos sistemas de información y monitoreo que optimizan la gestión operativa y social, utilizando tecnologías de punta para mejorar la eficiencia y la transparencia.</p>
                 </div>
     
                 {/* Tarjeta de Transformación Digital 2 */}
                 <div className={stylesSection.solutionItem}>
-                    <Image src={fondo} alt="Transformación Digital"/>
+                <Image
+                     src={require('../../../public/assets/img/home/sectionSomos/rocks-8283171_1920.jpg')} alt="Transformación Digital"/>
                     <h3>Transformación Digital</h3>
                     <p>Diseñamos e implementamos iniciativas que benefician a las comunidades en zonas de influencia, promoviendo el desarrollo sostenible y fortaleciendo las relaciones con los grupos de interés.</p>
                 </div>
     
                 {/* Tarjeta de Quality Due Diligence */}
                 <div className={stylesSection.solutionItem}>
-                    <Image src={fondo} alt="Quality Due Diligence"/>
+                <Image
+                     src={require('../../../public/assets/img/home/sectionSomos/rocks-8283171_1920.jpg')} alt="Quality Due Diligence"/>
                     <h3>Quality Due Diligence</h3>
                     <p>Ofrecemos servicios de auditoría y aseguramiento de la calidad para empresas globales, garantizando altos estándares y cumplimiento normativo.</p>
                 </div>
